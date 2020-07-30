395b48f169c323270bee6a2042667dbbd7137308
chrisbc <chrisbc@artisan.co.nz>
2020-07-30 15:30:32 +1200

    added get_ruptures feature with scale, aspect and fill_factor parameters

--- python/src/faultless.py
+++ python/src/faultless.py
@@ -1,6 +1,16 @@
 import csv
 from shapely.geometry import Point
+import numpy as np
 
+class FaultSubSectionFactory():
+
+    def __init__(self):
+        self.current_idx = 0
+
+    def new_fault_sub_section(self, parent):
+        fss =  FaultSubSection(self.current_idx, parent)
+        self.current_idx += 1
+        return fss 
 
 # TODO Docstrings
 class FaultSubSection():
@@ -8,12 +18,12 @@ class FaultSubSection():
     """
 
     @staticmethod
-    def from_csv_row(row, parent=None):
+    def from_csv_row(factory, row, parent=None):
         """
         row.headers along_strike_index,down_dip_index,lon1(deg),lat1(deg),
         lon2(deg),lat2(deg),dip (deg),top_depth (km),bottom_depth (km)
         """
-        fs = FaultSubSection(parent)
+        fs = factory.new_fault_sub_section(parent)
 
         fs._data = row
         fs._idx = (int(row['along_strike_index']), int(row['down_dip_index']))
@@ -23,15 +33,22 @@ class FaultSubSection():
         fs._dip = float(row['dip (deg)'])
         fs._top_depth = float(row['top_depth (km)'])
         fs._bottom_depth = float(row['bottom_depth (km)'])
+        
         return fs
 
-    def __init__(self, parent=None):
+    def __init__(self, id, parent=None):
+        self._id = id
         self._parent = parent
         self._data = None
         self._top_trace = None
 
     def __repr__(self):
         return "%s :: %s" % (self.__class__.__name__, self._top_trace)
+
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def parent(self):
@@ -61,10 +78,15 @@ class FaultSubSection():
 class SheetFault():
     def __init__(self, name):
         self._name = name
-        self._sub_sections = []
+        self._sub_sections = {}
+        self._column_max = 0
+        self._row_max = 0
+        #re-arrange the subsections as per their strike-dip_indices
+        self._grid = np.empty((20,20)) 
+        self._grid[:] = np.nan
         self._ruptures = []
 
-    def build_surface_from_csv(self, csv_data):
+    def build_surface_from_csv(self, factory, csv_data):
         reader = csv.DictReader(csv_data)
 
         # validate
@@ -73,9 +95,14 @@ class SheetFault():
             raise ValueError('Invalid header')
 
         for row in rows:
-            self._sub_sections.append(FaultSubSection
-                                      .from_csv_row(row, parent=self))
+            ss = FaultSubSection.from_csv_row(factory, row, parent=self)
+            self._sub_sections[ss.id] = ss
+            
+            idx = ss.strike_dip_index
 
+            self._column_max = max(self._column_max, idx[0])
+            self._row_max = max(self._row_max, idx[1])
+            self._grid[idx[0]][idx[1]] = ss.id
         return self
 
     @property
@@ -86,5 +113,30 @@ class SheetFault():
     def sub_sections(self):
         return self._sub_sections
 
-    def get_ruptures(self, min_size=1):
-        return self._ruptures
+    def get_ruptures(self, spec):
+
+        name = spec['name']
+        scale = spec['scale']
+        aspect = spec['aspect']
+
+        min_fill_factor = spec.get('min_fill_factor', 0.75)       
+        min_sections = int(scale * scale * aspect) * min_fill_factor 
+
+        def tuples_for_rupture_ids(ids):
+            res = []
+            for id in ids:
+                res.append( self.sub_sections[id].strike_dip_index )
+            return res
+
+        for col in range(0, self._column_max +1):
+            for row in range(0, self._row_max +1):
+                #get ruptures by range
+                rupt = self._grid[col:col+int(scale*aspect), row:row+scale]
+                #remove empty values
+                rupt = rupt[np.logical_not(np.isnan(rupt))]
+                #convert to integerrs and get a flat list
+                rupt = rupt.astype(int).flatten().tolist()
+                if len(rupt) and len(rupt) > min_sections:
+                    print(tuples_for_rupture_ids(rupt)) #.flatten().astype(int).tolist()))
+                    yield tuples_for_rupture_ids(rupt)  # 0.flatten().astype(int).tolist())
+
--- python/src/test/test_fault_section.py
+++ python/src/test/test_fault_section.py
@@ -1,6 +1,6 @@
 import unittest
 from io import StringIO
-from faultless import SheetFault, FaultSubSection
+from faultless import SheetFault, FaultSubSection, FaultSubSectionFactory
 import csv
 
 # fixtures
@@ -36,7 +36,7 @@ tile_param_csv = """along_strike_index,down_dip_index,lon1(deg),lat1(deg),lon2(d
 class TestSubductionZoneFault(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.factory = FaultSubSectionFactory()
 
     def test_create_new_fault(self):
         sf = SheetFault("My First Subduction Zone")
@@ -44,34 +44,38 @@ class TestSubductionZoneFault(unittest.TestCase):
         self.assertEqual(len(sf.sub_sections), 0)
 
     def test_load_sub_sections_from_csv(self):
+        
         sf = SheetFault("9 part Subduction Zone")\
-                .build_surface_from_csv(StringIO(tile_param_csv))
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
         self.assertEqual(len(sf.sub_sections), 24)
         self.assertIsInstance(sf.sub_sections[0], FaultSubSection)
 
-        print(sf.sub_sections[-1])
+        # print(sf.sub_sections.values[-1])
         self.assertIs(sf, sf.sub_sections[0].parent)
 
     def test_load_sub_sections_from_invalid_csv_exception(self):
         with self.assertRaises((ValueError, IndexError)):
             SheetFault("24 part Subduction Zone")\
-                .build_surface_from_csv(StringIO('Sorry this is not csv_data'))
+                .build_surface_from_csv(self.factory, StringIO('Sorry this is not csv_data'))
 
 
 class TestFaultSubSection(unittest.TestCase):
 
     def setUp(self):
+        self.factory = FaultSubSectionFactory()
+
         reader = csv.DictReader(StringIO(tile_param_csv))
         self.csvrows = [x for x in reader]
 
     def test_create_from_invalid_csvrow_exception(self):
         with self.assertRaises((KeyError,)):
-            FaultSubSection.from_csv_row(
+            FaultSubSection.from_csv_row(self.factory,
                     dict(x='Sorry this is not csv_data'))
 
     def test_create_from_csv_row(self):
-        fss = FaultSubSection.from_csv_row(self.csvrows[0], parent=None)
+        fss = FaultSubSection.from_csv_row(self.factory, self.csvrows[0], parent=None)
 
+        self.assertEqual(0, fss.id)
         self.assertAlmostEqual(-43.6606619468, fss.top_trace[0].x)
         self.assertAlmostEqual(172.550493842, fss.top_trace[0].y)
 
@@ -80,3 +84,71 @@ class TestFaultSubSection(unittest.TestCase):
 
         self.assertAlmostEqual(27.77718083, fss.top_depth)
         self.assertAlmostEqual(30.73264945, fss.bottom_depth)
+
+
+class TestGenerateRectangularRuptures(unittest.TestCase):
+    
+    def setUp(self):
+        self.factory = FaultSubSectionFactory()
+        self.sf = SheetFault("9 part Subduction Zone")\
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
+
+    # @unittest.skip("WIP") 
+    def test_rupture_one_by_one(self):
+        
+        shape_spec = dict(name="1 by 1", scale=1, aspect=1)
+        ruptures =  [r for r in self.sf.get_ruptures(shape_spec)]
+
+        self.assertEqual(ruptures[0], [(0, 0)])
+        self.assertEqual(ruptures[1], [(0, 1)])
+        self.assertEqual(ruptures[7], [(1, 0)])
+        self.assertEqual(ruptures[8], [(1, 1)])
+
+    def test_rupture_one_by_two(self):
+        sf = SheetFault("A subduction zone")\
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
+
+        shape_spec = dict(name="1 by 2", scale=1, aspect=2)
+        ruptures =  [r for r in self.sf.get_ruptures(shape_spec)]
+
+        self.assertEqual(ruptures[0], [(0, 0), (1, 0)]) #begin col 0 
+        self.assertEqual(ruptures[1], [(0, 1), (1, 1)])
+
+        self.assertEqual(ruptures[7], [(1, 0), (2, 0)]) #begin col 1
+        self.assertEqual(ruptures[8], [(1, 1), (2, 1)])
+
+    def test_rupture_two_by_one(self):
+        sf = SheetFault("A subduction zone")\
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
+
+        shape_spec = dict(name="1 by 2", scale=2, aspect=0.5)
+        ruptures =  [r for r in self.sf.get_ruptures(shape_spec)]
+
+        self.assertEqual(ruptures[0], [(0, 0), (0, 1)]) #begin col 0 
+        self.assertEqual(ruptures[5], [(0, 5), (0, 6)]) 
+
+        self.assertEqual(ruptures[6], [(1, 0), (1, 1)]) #begin col 1
+        self.assertEqual(ruptures[17], [(1, 11), (1, 12)])
+
+        self.assertEqual(ruptures[-1], [(2, 2), (2, 3)]) #last
+
+    def test_rupture_two_by_three(self):
+        sf = SheetFault("A subduction zone")\
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
+
+        shape_spec = dict(name="2 by 3", scale=2, aspect=1.5)
+        ruptures =  [r for r in self.sf.get_ruptures(shape_spec)]
+
+        self.assertEqual(ruptures[0], [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]) #begin col 0 
+
+    def test_rupture_four_by_four(self):
+        sf = SheetFault("A subduction zone")\
+                .build_surface_from_csv(self.factory, StringIO(tile_param_csv))
+
+        shape_spec = dict(name="4x4", scale=4, aspect=1, min_fill_factor=0.6)
+        ruptures =  [r for r in self.sf.get_ruptures(shape_spec)]
+
+        self.assertEqual(ruptures[0], [
+            (0, 0), (0, 1), (0, 2), (0, 3), 
+            (1, 0), (1, 1), (1, 2), (1, 3), 
+            (2, 0), (2, 1), (2, 2), (2, 3)]) 
