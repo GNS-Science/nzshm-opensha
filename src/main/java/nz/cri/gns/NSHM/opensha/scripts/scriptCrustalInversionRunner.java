3c1901b52391b4cc03c4228093e961f78797644d
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-10-14 11:25:42 +1300

    refacting tweaks; reinstate inversion support (short-term only);

--- src/main/java/nz/cri/gns/NSHM/opensha/scripts/scriptCrustalInversionRunner.java
+++ src/main/java/nz/cri/gns/NSHM/opensha/scripts/scriptCrustalInversionRunner.java
@@ -1,10 +1,10 @@
-/**
- * 
- */
 package nz.cri.gns.NSHM.opensha.scripts;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,13 +18,29 @@ import org.dom4j.DocumentException;
 
 import nz.cri.gns.NSHM.opensha.ruptures.NSHMRuptureSetBuilder;
 import nz.cri.gns.NSHM.opensha.ruptures.NSHMRuptureSetBuilder.RupturePermutationStrategy;
+import scratch.UCERF3.FaultSystemSolution;
 import scratch.UCERF3.SlipAlongRuptureModelRupSet;
+import scratch.UCERF3.inversion.UCERF3InversionConfiguration.SlipRateConstraintWeightingType;
+import scratch.UCERF3.simulatedAnnealing.ConstraintRange;
+import scratch.UCERF3.simulatedAnnealing.ThreadedSimulatedAnnealing;
+import scratch.UCERF3.simulatedAnnealing.completion.CompletionCriteria;
+import scratch.UCERF3.simulatedAnnealing.completion.TimeCompletionCriteria;
 import scratch.UCERF3.utils.FaultSystemIO;
+import scratch.UCERF3.utils.MFD_InversionConstraint;
+
+import org.opensha.sha.earthquake.faultSysSolution.inversion.InversionInputGenerator;
+import org.opensha.sha.earthquake.faultSysSolution.inversion.constraints.InversionConstraint;
+import org.opensha.sha.earthquake.faultSysSolution.inversion.constraints.impl.MFDEqualityInversionConstraint;
+import org.opensha.sha.earthquake.faultSysSolution.inversion.constraints.impl.MFDInequalityInversionConstraint;
+import org.opensha.sha.earthquake.faultSysSolution.inversion.constraints.impl.SlipRateInversionConstraint;
+import org.opensha.sha.magdist.GutenbergRichterMagFreqDist;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 /**
- * @author chrisbc
  *
- * initial testing of crustal inversions using the NZ CFM 0.3 XML
+ * Wrapper for building NSHM Rupture sets from CLI
  * 
  */
 public class scriptCrustalInversionRunner {
@@ -58,6 +74,11 @@ public class scriptCrustalInversionRunner {
 	public static void main(String[] args) throws DocumentException, IOException, ParseException {
 		CommandLine cmd = parseCommandLine(args);
 
+		boolean runInversion = true; // run inversion stage
+		long inversionMins = 1; // run it for this many minutes
+		long syncInterval = 10; // seconds between inversion synchronisations		
+		int numThreads = Runtime.getRuntime().availableProcessors(); // use all available processors
+		
 		File outputDir = new File(cmd.getOptionValue("outputDir")); 
 		File rupSetFile = new File(outputDir, "CFM_crustal_rupture_set.zip");
 		File solFile = new File(outputDir, "CFM_crustal_solution.zip");
@@ -109,111 +130,111 @@ public class scriptCrustalInversionRunner {
 		SlipAlongRuptureModelRupSet rupSet = builder.buildRuptureSet(fsdFile);
 		FaultSystemIO.writeRupSet(rupSet, rupSetFile);
 		
-//		if (cmd.hasOption("runInversion")) {
-//		System.out.println("set runInversion to " + cmd.getOptionValue("runInversion"));
-//		runInversion = Boolean.parseBoolean(cmd.getOptionValue("runInversion"));
-//	}
-//	if (cmd.hasOption("inversionMins")) {
-//		System.out.println("set inversionMins to " + cmd.getOptionValue("inversionMins"));
-//		inversionMins = Long.parseLong(cmd.getOptionValue("inversionMins"));
-//	}
-//	if (cmd.hasOption("syncInterval")) {
-//		System.out.println("set syncInterval to " + cmd.getOptionValue("syncInterval"));
-//		syncInterval = Long.parseLong(cmd.getOptionValue("syncInterval"));
-//	}
+		if (cmd.hasOption("runInversion")) {
+			System.out.println("set runInversion to " + cmd.getOptionValue("runInversion"));
+			runInversion = Boolean.parseBoolean(cmd.getOptionValue("runInversion"));
+		}
+		if (cmd.hasOption("inversionMins")) {
+			System.out.println("set inversionMins to " + cmd.getOptionValue("inversionMins"));
+			inversionMins = Long.parseLong(cmd.getOptionValue("inversionMins"));
+		}
+		if (cmd.hasOption("syncInterval")) {
+			System.out.println("set syncInterval to " + cmd.getOptionValue("syncInterval"));
+			syncInterval = Long.parseLong(cmd.getOptionValue("syncInterval"));
+		}
 			
-//		if (runInversion) {
-//			List<InversionConstraint> constraints = new ArrayList<>();
-//			
-//			/*
-//			 * Slip rate constraints
-//			 */
-//			// For SlipRateConstraintWeightingType.NORMALIZED (also used for SlipRateConstraintWeightingType.BOTH) -- NOT USED if UNNORMALIZED!
-//			double slipRateConstraintWt_normalized = 1;
-//			// For SlipRateConstraintWeightingType.UNNORMALIZED (also used for SlipRateConstraintWeightingType.BOTH) -- NOT USED if NORMALIZED!
-//			double slipRateConstraintWt_unnormalized = 100;
-//			// If normalized, slip rate misfit is % difference for each section (recommended since it helps fit slow-moving faults).
-//			// If unnormalized, misfit is absolute difference.
-//			// BOTH includes both normalized and unnormalized constraints.
-//			SlipRateConstraintWeightingType slipRateWeighting = SlipRateConstraintWeightingType.BOTH; // (recommended: BOTH)
-//			constraints.add(new SlipRateInversionConstraint(slipRateConstraintWt_normalized, slipRateConstraintWt_unnormalized,
-//					slipRateWeighting, rupSet, rupSet.getSlipRateForAllSections()));
-//			
-//			/*
-//			 * MFD constraints
-//			 */
-//			double totalRateM5 = 10d; // expected number of M>=5's per year
-//			double bValue = 1d; // G-R b-value
-//			// magnitude to switch from MFD equality to MFD inequality
-//			double mfdTransitionMag = 7.85;
-//			double mfdEqualityConstraintWt = 10;
-//			double mfdInequalityConstraintWt = 1000;
-//			int mfdNum = 40;
-//			double mfdMin = 5.05d;
-//			double mfdMax = 8.95;
-//			GutenbergRichterMagFreqDist mfd = new GutenbergRichterMagFreqDist(
-//					bValue, totalRateM5, mfdMin, mfdMax, mfdNum);
-//			int transitionIndex = mfd.getClosestXIndex(mfdTransitionMag);
-//			// snap it to the discretization if it wasn't already
-//			mfdTransitionMag = mfd.getX(transitionIndex);
-//			Preconditions.checkState(transitionIndex >= 0);
-//			GutenbergRichterMagFreqDist equalityMFD = new GutenbergRichterMagFreqDist(
-//					bValue, totalRateM5, mfdMin, mfdTransitionMag, transitionIndex);
-//			MFD_InversionConstraint equalityConstr = new MFD_InversionConstraint(equalityMFD, null);
-//			GutenbergRichterMagFreqDist inequalityMFD = new GutenbergRichterMagFreqDist(
-//					bValue, totalRateM5, mfdTransitionMag, mfdMax, mfd.size()-equalityMFD.size());
-//			MFD_InversionConstraint inequalityConstr = new MFD_InversionConstraint(inequalityMFD, null);
-//			
-//			constraints.add(new MFDEqualityInversionConstraint(rupSet, mfdEqualityConstraintWt,
-//					Lists.newArrayList(equalityConstr), null));
-//			constraints.add(new MFDInequalityInversionConstraint(rupSet, mfdInequalityConstraintWt,
-//					Lists.newArrayList(inequalityConstr)));
-//			
-//			// weight of entropy-maximization constraint (not used in UCERF3)
-//			double smoothnessWt = 0;
-//			
-//			/*
-//			 * Build inversion inputs
-//			 */
-//			InversionInputGenerator inputGen = new InversionInputGenerator(rupSet, constraints);
-//			
-//			inputGen.generateInputs(true);
-//			// column compress it for fast annealing
-//			inputGen.columnCompress();
-//			
-//			// inversion completion criteria (how long it will run)
-//			CompletionCriteria criteria = TimeCompletionCriteria.getInMinutes(inversionMins);
-//			
-//			// Bring up window to track progress
-//			// criteria = new ProgressTrackingCompletionCriteria(criteria, progressReport, 0.1d);
-//			
-//			// this is the "sub completion criteria" - the amount of time (or iterations) between synchronization
-//			CompletionCriteria subCompletionCriteria = TimeCompletionCriteria.getInSeconds(syncInterval); // 1 second;
-//			
-//			ThreadedSimulatedAnnealing tsa = new ThreadedSimulatedAnnealing(inputGen.getA(), inputGen.getD(),
-//					inputGen.getInitialSolution(), smoothnessWt, inputGen.getA_ineq(), inputGen.getD_ineq(),
-//					inputGen.getWaterLevelRates(), numThreads, subCompletionCriteria);
-//			tsa.setConstraintRanges(inputGen.getConstraintRowRanges());
-//			
-//			tsa.iterate(criteria);
-//			
-//			// now assemble the solution
-//			double[] solution_raw = tsa.getBestSolution();
-//			
-//			// adjust for minimum rates if applicable
-//			double[] solution_adjusted = inputGen.adjustSolutionForWaterLevel(solution_raw);
-//			
-//			Map<ConstraintRange, Double> energies = tsa.getEnergies();
-//			if (energies != null) {
-//				System.out.println("Final energies:");
-//				for (ConstraintRange range : energies.keySet())
-//					System.out.println("\t"+range.name+": "+energies.get(range).floatValue());
-//			}
-//			
-//			// now write out the solution
-//			FaultSystemSolution sol = new FaultSystemSolution(rupSet, solution_adjusted);
-//			FaultSystemIO.writeSol(sol, solFile);
-//		}		
+		if (runInversion) {
+			List<InversionConstraint> constraints = new ArrayList<>();
+			
+			/*
+			 * Slip rate constraints
+			 */
+			// For SlipRateConstraintWeightingType.NORMALIZED (also used for SlipRateConstraintWeightingType.BOTH) -- NOT USED if UNNORMALIZED!
+			double slipRateConstraintWt_normalized = 1;
+			// For SlipRateConstraintWeightingType.UNNORMALIZED (also used for SlipRateConstraintWeightingType.BOTH) -- NOT USED if NORMALIZED!
+			double slipRateConstraintWt_unnormalized = 100;
+			// If normalized, slip rate misfit is % difference for each section (recommended since it helps fit slow-moving faults).
+			// If unnormalized, misfit is absolute difference.
+			// BOTH includes both normalized and unnormalized constraints.
+			SlipRateConstraintWeightingType slipRateWeighting = SlipRateConstraintWeightingType.BOTH; // (recommended: BOTH)
+			constraints.add(new SlipRateInversionConstraint(slipRateConstraintWt_normalized, slipRateConstraintWt_unnormalized,
+					slipRateWeighting, rupSet, rupSet.getSlipRateForAllSections()));
+			
+			/*
+			 * MFD constraints
+			 */
+			double totalRateM5 = 10d; // expected number of M>=5's per year
+			double bValue = 1d; // G-R b-value
+			// magnitude to switch from MFD equality to MFD inequality
+			double mfdTransitionMag = 7.85;
+			double mfdEqualityConstraintWt = 10;
+			double mfdInequalityConstraintWt = 1000;
+			int mfdNum = 40;
+			double mfdMin = 5.05d;
+			double mfdMax = 8.95;
+			GutenbergRichterMagFreqDist mfd = new GutenbergRichterMagFreqDist(
+					bValue, totalRateM5, mfdMin, mfdMax, mfdNum);
+			int transitionIndex = mfd.getClosestXIndex(mfdTransitionMag);
+			// snap it to the discretization if it wasn't already
+			mfdTransitionMag = mfd.getX(transitionIndex);
+			Preconditions.checkState(transitionIndex >= 0);
+			GutenbergRichterMagFreqDist equalityMFD = new GutenbergRichterMagFreqDist(
+					bValue, totalRateM5, mfdMin, mfdTransitionMag, transitionIndex);
+			MFD_InversionConstraint equalityConstr = new MFD_InversionConstraint(equalityMFD, null);
+			GutenbergRichterMagFreqDist inequalityMFD = new GutenbergRichterMagFreqDist(
+					bValue, totalRateM5, mfdTransitionMag, mfdMax, mfd.size()-equalityMFD.size());
+			MFD_InversionConstraint inequalityConstr = new MFD_InversionConstraint(inequalityMFD, null);
+			
+			constraints.add(new MFDEqualityInversionConstraint(rupSet, mfdEqualityConstraintWt,
+					Lists.newArrayList(equalityConstr), null));
+			constraints.add(new MFDInequalityInversionConstraint(rupSet, mfdInequalityConstraintWt,
+					Lists.newArrayList(inequalityConstr)));
+			
+			// weight of entropy-maximization constraint (not used in UCERF3)
+			double smoothnessWt = 0;
+			
+			/*
+			 * Build inversion inputs
+			 */
+			InversionInputGenerator inputGen = new InversionInputGenerator(rupSet, constraints);
+			
+			inputGen.generateInputs(true);
+			// column compress it for fast annealing
+			inputGen.columnCompress();
+			
+			// inversion completion criteria (how long it will run)
+			CompletionCriteria criteria = TimeCompletionCriteria.getInMinutes(inversionMins);
+			
+			// Bring up window to track progress
+			// criteria = new ProgressTrackingCompletionCriteria(criteria, progressReport, 0.1d);
+			
+			// this is the "sub completion criteria" - the amount of time (or iterations) between synchronization
+			CompletionCriteria subCompletionCriteria = TimeCompletionCriteria.getInSeconds(syncInterval); // 1 second;
+			
+			ThreadedSimulatedAnnealing tsa = new ThreadedSimulatedAnnealing(inputGen.getA(), inputGen.getD(),
+					inputGen.getInitialSolution(), smoothnessWt, inputGen.getA_ineq(), inputGen.getD_ineq(),
+					inputGen.getWaterLevelRates(), numThreads, subCompletionCriteria);
+			tsa.setConstraintRanges(inputGen.getConstraintRowRanges());
+			
+			tsa.iterate(criteria);
+			
+			// now assemble the solution
+			double[] solution_raw = tsa.getBestSolution();
+			
+			// adjust for minimum rates if applicable
+			double[] solution_adjusted = inputGen.adjustSolutionForWaterLevel(solution_raw);
+			
+			Map<ConstraintRange, Double> energies = tsa.getEnergies();
+			if (energies != null) {
+				System.out.println("Final energies:");
+				for (ConstraintRange range : energies.keySet())
+					System.out.println("\t"+range.name+": "+energies.get(range).floatValue());
+			}
+			
+			// now write out the solution
+			FaultSystemSolution sol = new FaultSystemSolution(rupSet, solution_adjusted);
+			FaultSystemIO.writeSol(sol, solFile);
+		}		
 	
 		System.exit(0);
 	
