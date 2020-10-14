242d36524e88e97e194a151b643b5c576354e4c2
Chris Chamberlain <chrisbc@artisan.co.nz>
2020-10-15 07:53:52 +1300

    improved the docs

--- src/main/java/nz/cri/gns/NSHM/opensha/util/NSHMRuptureSetBuilderPythonGateway.java
+++ src/main/java/nz/cri/gns/NSHM/opensha/util/NSHMRuptureSetBuilderPythonGateway.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import org.dom4j.DocumentException;
 
 import nz.cri.gns.NSHM.opensha.ruptures.NSHMRuptureSetBuilder;
-import nz.cri.gns.NSHM.opensha.ruptures.NSHMRuptureSetBuilder.RupturePermutationStrategy;
 import py4j.GatewayServer;
 import scratch.UCERF3.SlipAlongRuptureModelRupSet;
 import scratch.UCERF3.utils.FaultSystemIO;
 
-
+/**
+ * A py4j gateway for the NSHMRuptureSetBuilder.
+ *  
+ */
 public class NSHMRuptureSetBuilderPythonGateway {
 
 	static CachedNSHMRuptureSetBuilder builder = new CachedNSHMRuptureSetBuilder();
@@ -27,20 +29,38 @@ public class NSHMRuptureSetBuilderPythonGateway {
 		server.start();
 	}	
 
+	/**
+	 * Provide a little help for python clients using NSHMRuptureSetBuilder
+	 * 
+	 */
 	static class CachedNSHMRuptureSetBuilder extends NSHMRuptureSetBuilder {
 		SlipAlongRuptureModelRupSet ruptureSet;
 
+		/**
+		 * 
+		 * @param permutationStrategyClass one of 'DOWNDIP', 'POINTS', 'UCERF3'
+		 * @return this
+		 */
 		public NSHMRuptureSetBuilder setPermutationStrategy(String permutationStrategyClass) {
+		 
 			super.setPermutationStrategy(RupturePermutationStrategy.valueOf(permutationStrategyClass));
 			return this;
 		}
 		
+		/**
+		 * Caches the results of the build
+		 */
 		@Override
 		public SlipAlongRuptureModelRupSet buildRuptureSet(String fsdFileName) throws DocumentException, IOException {
 			ruptureSet = super.buildRuptureSet(fsdFileName);
 			return ruptureSet;
 		}
-		
+		/**
+		 * Write the cached rupture set to disk.
+		 *  
+		 * @param rupSetFileName
+		 * @throws IOException
+		 */
 		public void writeRuptureSet(String rupSetFileName) throws IOException {
 			File rupSetFile = new File(rupSetFileName);
 			FaultSystemIO.writeRupSet(ruptureSet, rupSetFile);
