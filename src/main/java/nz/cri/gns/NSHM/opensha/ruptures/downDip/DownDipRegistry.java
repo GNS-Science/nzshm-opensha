f9418bb98714c550c3aeea558098df09887a9a96
voj <o.jurgens@gns.cri.nz>
2021-01-06 17:00:34 +1300

    docstrings

--- src/main/java/nz/cri/gns/NSHM/opensha/ruptures/downDip/DownDipRegistry.java
+++ src/main/java/nz/cri/gns/NSHM/opensha/ruptures/downDip/DownDipRegistry.java
@@ -11,16 +11,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 
+/**
+ * This class hides some of the messiness of hacking downdip faults into the framework.
+ */
 public class DownDipRegistry {
 
     final HashMap<Integer, DownDipSubSectBuilder> builders;
     final FaultSectionList subSections;
 
+    /**
+     * Creates a new DownDipRegistry. There should be only one in your application.
+     *
+     * @param subSections A list of all FaultSections, downdip or crustal. All downdip Faultsections
+     *                    will be added to this.
+     */
     public DownDipRegistry(FaultSectionList subSections) {
         this.subSections = subSections;
         this.builders = new HashMap<>();
     }
 
+    /**
+     * Loads a downdip fault from a CSV file and adds all sections to the subSections list
+     * passed to the registry constructor.
+     *
+     * @param id      The imaginary parent fault id.
+     * @param name    The name of the fault.
+     * @param csvFile The CSV file.
+     * @throws IOException
+     */
     public void loadFromFile(int id, String name, File csvFile) throws IOException {
         FaultSectionPrefData interfaceParentSection = new FaultSectionPrefData();
         interfaceParentSection.setSectionId(id);
@@ -37,19 +55,34 @@ public class DownDipRegistry {
         }
     }
 
-    public boolean isDownDip(FaultSubsectionCluster cluster){
+    /**
+     * Returns true if the cluster's parent id is a downdip parent id
+     *
+     * @param cluster the cluster
+     * @return whether the cluster is part of a downdip fault.
+     */
+    public boolean isDownDip(FaultSubsectionCluster cluster) {
         return builders.containsKey(cluster.parentSectionID);
     }
 
-    public boolean isDownDip(FaultSection subSection){
+    /**
+     * Returns true if the subsection's parent id is a downdip parent id
+     *
+     * @param subSection the FaultSection
+     * @return whether the cluster is part of a downdip fault.
+     */
+    public boolean isDownDip(FaultSection subSection) {
         return builders.containsKey(subSection.getParentSectionId());
     }
 
-    public DownDipSubSectBuilder getBuilder(int id){
+    /**
+     * Returns the builder for the specified downdip id.
+     * The builder can be queried for downdip meta data.
+     *
+     * @param id the id
+     * @return the builder matching the id
+     */
+    public DownDipSubSectBuilder getBuilder(int id) {
         return builders.get(id);
-    }
-
-    public DownDipSubSectBuilder getBuilder(FaultSubsectionCluster cluster){
-        return builders.get(cluster.parentSectionID);
     }
 }
