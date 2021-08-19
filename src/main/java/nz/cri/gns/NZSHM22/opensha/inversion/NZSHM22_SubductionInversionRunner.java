0bd60f90a18c365259f26e8ad491eec3c59ba361
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-08-19 15:31:09 +1200

    tweaks to local config

--- src/main/java/nz/cri/gns/NZSHM22/opensha/inversion/NZSHM22_SubductionInversionRunner.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/inversion/NZSHM22_SubductionInversionRunner.java
@@ -74,20 +74,36 @@ public class NZSHM22_SubductionInversionRunner extends NZSHM22_AbstractInversion
 
 	public static void main(String[] args) throws IOException, DocumentException {
 
-		File inputDir = new File("c:/Users/volkertj/Downloads/");
-		File outputRoot = new File("c:/tmp/");
+		File inputDir = new File("./TEST");
+		File outputRoot = new File("./TEST");
 		File ruptureSet = new File(inputDir,
 				"RupSet_Sub_FM(SBD_0_2A_HKR_LR_30)_mnSbS(2)_mnSSPP(2)_mxSSL(0.5)_ddAsRa(2.0,5.0,5)_ddMnFl(0.1)_ddPsCo(0.0)_ddSzCo(0.0)_thFc(0.0).zip");
 		File outputDir = new File(outputRoot, "inversions");
 		Preconditions.checkState(outputDir.exists() || outputDir.mkdir());
 
+		/*
+		 * NZSHM22_InversionSolution-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjIzMzliekRWcw==.zip
+		 * mfd_equality_weight	1000.0
+		 * mfd_inequality_weight	10000.0
+		 * slip_rate_weighting_type	BOTH
+		 * slip_rate_normalized_weight	1000.0
+		 * slip_rate_unnormalized_weight	10000.0
+		 * mfd_mag_gt_5	29
+		 * mfd_b_value	1.05
+		 * mfd_transition_mag	9.15	
+		 */
 		NZSHM22_SubductionInversionRunner runner = ((NZSHM22_SubductionInversionRunner) new NZSHM22_SubductionInversionRunner()
-				.setInversionSeconds(10).setNumThreads(1).setRuptureSetFile(ruptureSet)
-				.setGutenbergRichterMFDWeights(100.0, 1000.0)) // end super-class methods
-				.setGutenbergRichterMFD(2.4, 1.3, 7.75).configure();
+				.setInversionSeconds(10).setNumThreads(5).setRuptureSetFile(ruptureSet)
+				.setGutenbergRichterMFDWeights(1000.0, 10000.0)
+				.setSlipRateConstraint("BOTH", 1000.0, 10000.0)
+				) // end super-class methods
+				.setGutenbergRichterMFD(29, 1.05, 9.15).configure();
 
-		NZSHM22_InversionFaultSystemSolution solution = runner.runInversion();
-		File solutionFile = new File(outputDir, "OldFormatSubductionInversionSolution.zip");
+		NZSHM22_InversionFaultSystemSolution solution = runner
+				.setInversionSeconds(30)
+				.setNumThreads(4)
+				.runInversion();
+		File solutionFile = new File(outputDir, "OldFormatSubductionInversionSolution-RWcw.zip");
 		
 		System.out.println("Solution MFDS...");
 		for (ArrayList<String> row: runner.getTabularSolutionMfds()) {
@@ -97,7 +113,7 @@ public class NZSHM22_SubductionInversionRunner extends NZSHM22_AbstractInversion
 		// old-fashioned format
 		U3FaultSystemIO.writeSol(solution, solutionFile);
 
-		solution.write(new File(outputDir, "NewFormatSubductionInversionSolution.zip"));
+		solution.write(new File(outputDir, "NewFormatSubductionInversionSolution-RWcw.zip"));
 
 	}
 
