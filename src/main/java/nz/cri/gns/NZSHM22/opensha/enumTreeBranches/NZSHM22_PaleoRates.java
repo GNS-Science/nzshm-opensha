ff32aa75f0239e5cacc1e18b27d38c371b8b85b5
Chris B Chamberlain <chrisbc@artisan.co.nz>
2022-03-09 14:23:15 +1300

    Feature/166 new paleo timing models (#198)
    * new paleo timing models
    * test and tidy up

--- src/main/java/nz/cri/gns/NZSHM22/opensha/enumTreeBranches/NZSHM22_PaleoRates.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/enumTreeBranches/NZSHM22_PaleoRates.java
@@ -1,6 +1,7 @@
 package nz.cri.gns.NZSHM22.opensha.enumTreeBranches;
 
 import nz.cri.gns.NZSHM22.opensha.util.SimpleGeoJsonBuilder;
+
 import org.opensha.commons.data.CSVFile;
 import org.opensha.commons.data.uncertainty.BoundedUncertainty;
 import org.opensha.commons.data.uncertainty.UncertaintyBoundType;
@@ -20,14 +21,18 @@ import java.util.Map;
 
 public enum NZSHM22_PaleoRates implements LogicTreeNode {
 
-    GEOLOGIC_SLIP_1_0(
-    		"Geologic v1",
-    		"NZNSHM_paleotimings_GEOLOGICsliprates_all_1.0.csv"),
+    GEODETIC_SLIP_PRIOR_22FEB(
+    		"Geodetic with Geologic prior timing, 22 Feb 2022",
+    		"NZNSHM_paleotimings_GEODETICGEOLOGICPRIOR_22feb.txt"),
     
-    GEODETIC_SLIP_1_0(
-    		"Geodetic v1",
-    		"NZNSHM_paleotimings_GEODETICsliprates_1.0.csv"),
-
+    GEODETIC_SLIP_22FEB(
+    		"Geodetic timing, 22 Feb 2022",
+    		"NZNSHM_paleotimings_GEODETICsliprates_22feb.txt"),
+    
+    GEOLOGIC_SLIP_22FEB(
+    		"Geologic timing, 22 Feb 2022",
+    		"NZNSHM_paleotimings_GEOLOGICsliprates_22feb.txt"),
+	
     GEODETIC_SLIP_PRIOR_4FEB(
     		"Geodetic with Geologic prior timing, 4 Feb 2022",
     		"NZNSHM_paleotimings_GEODETICGEOLOGICPRIOR_4feb.csv"),
@@ -38,7 +43,15 @@ public enum NZSHM22_PaleoRates implements LogicTreeNode {
     
     GEOLOGIC_SLIP_4FEB(
     		"Geologic timing, 4 Feb 2022",
-    		"NZNSHM_paleotimings_GEOLOGICsliprates_4feb.csv");
+    		"NZNSHM_paleotimings_GEOLOGICsliprates_4feb.csv"),
+
+    GEOLOGIC_SLIP_1_0(
+    		"Geologic v1",
+    		"NZNSHM_paleotimings_GEOLOGICsliprates_all_1.0.csv"),
+    
+    GEODETIC_SLIP_1_0(
+    		"Geodetic v1",
+    		"NZNSHM_paleotimings_GEODETICsliprates_1.0.csv");
 
     final static String RESOURCE_PATH = "/paleoRates/";
 
--- src/main/java/nz/cri/gns/NZSHM22/opensha/inversion/NZSHM22_CrustalInversionRunner.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/inversion/NZSHM22_CrustalInversionRunner.java
@@ -207,11 +207,8 @@ public class NZSHM22_CrustalInversionRunner extends NZSHM22_AbstractInversionRun
         File inputDir = new File("./TEST");
         File outputRoot = new File("./TEST");
         File ruptureSet = new File(
-                "C:\\Users\\volkertj\\Downloads\\NZSHM22_RuptureSet-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjc5OTBvWWZMVw==(1).zip");
-    //            "C:\\Users\\volkertj\\Downloads\\NZSHM22_RuptureSet-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjU0MjJKaFdxVg==(3).zip");
-                //"C:\\Users\\volkertj\\Downloads\\RupSet_Cl_FM(CFM_0_9_SANSTVZ_D90)_mnSbS(2)_mnSSPP(2)_mxSSL(0.5)_mxFS(2000)_noInP(T)_slRtP(0.05)_slInL(F)_cfFr(0.75)_cfRN(2)_cfRTh(0.5)_cfRP(0.01)_fvJm(T)_jmPTh(0.001)_cmRkTh(360)_mxJmD(15)_plCn(T)_adMnD(6)_adScFr(0.2)_.zip");
-//				"RupSet_Cl_FM(CFM_0_9_SANSTVZ_2010)_mnSbS(2)_mnSSPP(2)_mxSSL(0.5)_mxFS(2000)_noInP(T)_slRtP(0.05)_slInL(F)_cfFr(0.75)_cfRN(2)_cfRTh(0.5)_cfRP(0.01)_fvJm(T)_jmPTh(0.001)_cmRkTh(360)_mxJmD(15)_plCn(T)_adMnD(6)_adScFr(0.2).zip");
-//        		"C:\\Users\\volkertj\\Downloads\\RupSet_Cl_FM(CFM_0_9_SANSTVZ_D90)_noInP(T)_slRtP(0.05)_slInL(F)_cfFr(0.75)_cfRN(2)_cfRTh(0.5)_cfRP(0.01)_fvJm(T)_jmPTh(0.001)_cmRkTh(360)_mxJmD(15)_plCn(T)_adMnD(6)_adScFr(0)_bi(F)_stGrSp(2)_coFr(0.5)(5).zip");
+//                "C:\\Users\\volkertj\\Downloads\\NZSHM22_RuptureSet-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjc5OTBvWWZMVw==(1).zip");
+        		"./TEST/NZSHM22_RuptureSet-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjg5ODJGamtLRw==.zip"); //Latest Prod
         File outputDir = new File(outputRoot, "inversions");
         Preconditions.checkState(outputDir.exists() || outputDir.mkdir());
 
@@ -219,20 +216,24 @@ public class NZSHM22_CrustalInversionRunner extends NZSHM22_AbstractInversionRun
         scaling.setupCrustal(4.2, 4.2);
 
         NZSHM22_CrustalInversionRunner runner = ((NZSHM22_CrustalInversionRunner) new NZSHM22_CrustalInversionRunner()
-                .setMaxMags("FILTER_RUPSET",10,7.5)
+
+                .setMaxMags("MANIPULATE_MFD",10,7.5)//FILTER_RUPSET
                 .setMinMags(6.8 , 6.5)
               //  .setInitialSolution("C:\\tmp\\rates.csv")
                 .setInversionSeconds(1)
+              // .setSlipRateFactor(0.8, 0.3)
+
                 .setScalingRelationship(scaling, true)
-             //   .setDeformationModel("GEOD_NO_PRIOR_UNISTD_2010_RmlsZTo4NTkuMDM2Z2Rw")
+                //.setDeformationModel("GEOD_NO_PRIOR_UNISTD_2010_RmlsZTo4NTkuMDM2Z2Rw")
                 .setRuptureSetFile(ruptureSet)
+
                // .setGutenbergRichterMFDWeights(100.0, 1000.0)
                 .setUncertaintyWeightedMFDWeights(10000, .75, 0.4)
-            //    .setSlipRateConstraint("BOTH", 1000, 1000)
+                //.setSlipRateConstraint("BOTH", 1000, 1000)
                 .setSlipRateUncertaintyConstraint(1000, 2)
                 .setReweightTargetQuantity("MAD"))
                 .setGutenbergRichterMFD(3.9, 1.0, 0.9, 1.2, 7.85)
-                .setPaleoRateConstraints(0.01, 1000, "GEODETIC_SLIP_1_0", "UCERF3_PLUS_PT25");
+                .setPaleoRateConstraints(0.01, 1000, "GEOLOGIC_SLIP_22FEB", "UCERF3_PLUS_PT25");
 
         FaultSystemSolution solution = runner.runInversion();
 
@@ -245,12 +246,14 @@ public class NZSHM22_CrustalInversionRunner extends NZSHM22_AbstractInversionRun
         for (ArrayList<String> row : runner.getTabularSolutionMfds()) {
             System.out.println(row);
         }
+        System.out.println("Solution MFDS V2 ...");      
+        for (ArrayList<String> row : runner.getTabularSolutionMfdsV2()) {
+            System.out.println(row);
+        }        
 //		System.out.println(solution.getEnergies().toString());
 
         File solutionFile = new File(outputDir, "CrustalInversionSolution.zip");
         solution.write(solutionFile);
-
-//	U3FaultSystemIO.writeSol(solution, solutionFile);
 
     }
 
