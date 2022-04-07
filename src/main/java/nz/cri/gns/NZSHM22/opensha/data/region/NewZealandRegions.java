0d112bd79293bb3eee76371159c906ba6674763b
voj <o.jurgens@gns.cri.nz>
2022-04-07 15:50:20 +1200

    [#139] up-sampled grids

--- src/main/java/nz/cri/gns/NZSHM22/opensha/data/region/NewZealandRegions.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/data/region/NewZealandRegions.java
@@ -1,6 +1,8 @@
 package nz.cri.gns.NZSHM22.opensha.data.region;
 
 import java.io.BufferedReader;
+
+import nz.cri.gns.NZSHM22.opensha.griddedSeismicity.NZSHM22_GriddedData;
 import org.opensha.commons.geo.BorderType;
 import org.opensha.commons.geo.GriddedRegion;
 import org.opensha.commons.geo.Location;
@@ -12,6 +14,8 @@ import nz.cri.gns.NZSHM22.opensha.util.NZSHM22_DataUtils;
 public class NewZealandRegions {
 
 	static String DATA_DIR = "region";
+
+	public final static GriddedRegion NZ = new NZ_RECTANGLE_GRIDDED();
 
 	private NewZealandRegions() {
 	};
@@ -47,7 +51,7 @@ public class NewZealandRegions {
 	 */
 	public static final class NZ_TEST_GRIDDED extends GriddedRegion {
 		public NZ_TEST_GRIDDED() {
-			super(readCoords("nz_test.coords"), BorderType.MERCATOR_LINEAR, 0.1, ANCHOR_0_0);
+			super(readCoords("nz_test.coords"), BorderType.MERCATOR_LINEAR, NZSHM22_GriddedData.GRID_SPACING, ANCHOR_0_0);
 			this.setName("NZTEST Gridded Region");
 		}
 	}
@@ -74,7 +78,7 @@ public class NewZealandRegions {
 
 	public static final class NZ_TVZ_GRIDDED extends GriddedRegion {
 		public NZ_TVZ_GRIDDED() {
-			super((Region) new NZ_TVZ(), 0.1, ANCHOR_0_0);
+			super((Region) new NZ_TVZ(), NZSHM22_GriddedData.GRID_SPACING, ANCHOR_0_0);
 			this.setName("NZ TVZ Gridded Region");
 		}
 	}
@@ -93,7 +97,7 @@ public class NewZealandRegions {
 
 	public static final class NZ_RECTANGLE_SANS_TVZ_GRIDDED extends GriddedRegion {
 		public NZ_RECTANGLE_SANS_TVZ_GRIDDED() {
-			super((Region) new NZ_RECTANGLE_SANS_TVZ(), 0.1, ANCHOR_0_0);
+			super((Region) new NZ_RECTANGLE_SANS_TVZ(), NZSHM22_GriddedData.GRID_SPACING, ANCHOR_0_0);
 			this.setName("NZ RECTANGLE SANS TVZ Gridded Region");
 		}
 	}
@@ -110,7 +114,7 @@ public class NewZealandRegions {
 
 	public static final class NZ_RECTANGLE_GRIDDED extends GriddedRegion {
 		public NZ_RECTANGLE_GRIDDED() {
-			super(readCoords("nz_rectangle.coords"), BorderType.MERCATOR_LINEAR, 0.1, ANCHOR_0_0);
+			super(readCoords("nz_rectangle.coords"), BorderType.MERCATOR_LINEAR, NZSHM22_GriddedData.GRID_SPACING, ANCHOR_0_0);
 			this.setName("NZ_RECTANGLE Gridded Region");
 		}
 	}
--- src/test/java/nz/cri/gns/NZSHM22/opensha/griddedSeismicity/NZSHM22_GriddedDataTest.java
+++ src/test/java/nz/cri/gns/NZSHM22/opensha/griddedSeismicity/NZSHM22_GriddedDataTest.java
@@ -1,9 +1,12 @@
 package nz.cri.gns.NZSHM22.opensha.griddedSeismicity;
 
 import nz.cri.gns.NZSHM22.opensha.data.region.NewZealandRegions;
+import nz.cri.gns.NZSHM22.opensha.enumTreeBranches.NZSHM22_SpatialSeisPDF;
 import org.junit.Test;
 import org.opensha.commons.geo.GriddedRegion;
 import org.opensha.commons.geo.Location;
+
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -13,8 +16,9 @@ public class NZSHM22_GriddedDataTest {
 
     @Test
     public void testNormaliseRegion(){
-        NZSHM22_GriddedData data = new NZSHM22_GriddedData("BEST2FLTOLDNC1246.txt");
-        GriddedRegion region = new NewZealandRegions.NZ_TVZ_GRIDDED();
+        NZSHM22_GriddedData data = NZSHM22_GriddedData.fromFileNativeStep("seismicityGrids/BEST2FLTOLDNC1246.txt");
+        // create a gridded region that matches the spacing of the grid
+        GriddedRegion region = new GriddedRegion(new NewZealandRegions.NZ_TVZ(), 1.0 / data.getStep(), GriddedRegion.ANCHOR_0_0);
 
         Location testLocation = region.getLocation(5);
         double locationPdf = data.getValue(testLocation);
@@ -32,9 +36,9 @@ public class NZSHM22_GriddedDataTest {
     @Test
     public void testNormaliseMultipleRegions(){
         // note: the regions may not share nodes
-        NZSHM22_GriddedData data = new NZSHM22_GriddedData("BEST2FLTOLDNC1246.txt");
-        GriddedRegion tvz = new NewZealandRegions.NZ_TVZ_GRIDDED();
-        GriddedRegion sansTvz = new NewZealandRegions.NZ_RECTANGLE_SANS_TVZ_GRIDDED();
+        NZSHM22_GriddedData data = NZSHM22_GriddedData.fromFileNativeStep("seismicityGrids/BEST2FLTOLDNC1246.txt");
+        GriddedRegion tvz = new GriddedRegion(new NewZealandRegions.NZ_TVZ(), 1.0 / data.getStep(), GriddedRegion.ANCHOR_0_0);
+        GriddedRegion sansTvz = new GriddedRegion(new NewZealandRegions.NZ_RECTANGLE_SANS_TVZ(), 1.0 / data.getStep(), GriddedRegion.ANCHOR_0_0);
 
         Location tvzLocation = tvz.getLocation(5);
         double tnzPdf = data.getValue(tvzLocation);
@@ -59,5 +63,35 @@ public class NZSHM22_GriddedDataTest {
         assertEquals( sansPdf / sansFraction, data.getValue(sansLocation), tolerance );
         assertEquals( 1, data.getFractionInRegion(sansTvz), tolerance);
 
+    }
+
+    @Test
+    public void testSerialisation() throws IOException {
+        NZSHM22_GriddedData original = NZSHM22_SpatialSeisPDF.NZSHM22_1346.getGriddedData();
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        original.writeToStream(new BufferedOutputStream(out));
+        out.close();
+        NZSHM22_GriddedData actual = new NZSHM22_GriddedData();
+        actual.initFromStream(new BufferedInputStream(new ByteArrayInputStream(out.toByteArray())));
+
+        GriddedRegion region = new NewZealandRegions.NZ_RECTANGLE_GRIDDED();
+
+        assertArrayEquals(original.getValues(region), actual.getValues(region), 0.000000000000001);
+    }
+
+    @Test
+    public void testUpsampling(){
+        NZSHM22_GriddedData data = NZSHM22_GriddedData.fromFileNativeStep("seismicityGrids/BEST2FLTOLDNC1246.txt");
+        assertEquals(10, data.getStep(), 0);
+
+        NZSHM22_GriddedData upSampled = new NZSHM22_GriddedData(data, 100);
+
+        GriddedRegion nz100 = new GriddedRegion(new NewZealandRegions.NZ_RECTANGLE(), 1.0 / 100, GriddedRegion.ANCHOR_0_0);
+
+        double[] values10 = data.getValues(nz100);
+        double[] values100 = upSampled.getValues(nz100);
+
+        assertArrayEquals(values10, values100, 0.000000000001);
     }
 }
