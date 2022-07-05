f4bbbf3c6971906cf58674dd87d03c59e6a4a2d9
voj <o.jurgens@gns.cri.nz>
2022-07-05 14:46:34 +1200

    disable cumulative jump distance

--- src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/NZSHM22_CoulombRuptureSetBuilder.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/NZSHM22_CoulombRuptureSetBuilder.java
@@ -705,7 +705,7 @@ public class NZSHM22_CoulombRuptureSetBuilder extends NZSHM22_AbstractRuptureSet
                 .setAdaptiveMinDist(6.0)
                 .setAdaptiveSectFract(0)
                 .setMaxJumpDistance(15)
-                .setSlipRateProb(0)
+                .setJumpProbThresh(0)
                 .setMinSubSections(2)
                 .setMinSubSectsPerParent(2)
                 .setScaleDepthExcludeDomain("4", 0.8)
