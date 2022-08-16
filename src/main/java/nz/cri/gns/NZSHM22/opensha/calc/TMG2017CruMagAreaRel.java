73a766553baa51d67cc3d4627410b8f688b8888e
voj <o.jurgens@gns.cri.nz>
2022-08-16 13:46:59 +1200

    [#275] move Kiran's scaling relationships out of opensha

---
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/calc/TMG2017CruMagAreaRel.java
@@ -0,0 +1,144 @@
+package nz.cri.gns.NZSHM22.opensha.calc;
+
+import org.opensha.commons.calc.magScalingRelations.MagAreaRelationship;
+
+import static nz.cri.gns.NZSHM22.opensha.calc.TMG2017FaultingType.*;
+
+
+/**
+ * <b>Title:</b>TMG2017_MagAreaRel<br>
+ *
+ * <b>Description:</b>
+ * <p>
+ * This implements CRUSTAL faults specific magnitude versus rupture area
+ * relations of Thingbaijam K.K.S., P.M. Mai and K. Goda 2017, Bull. Seism. Soc.
+ * Am., 107, 2225–2246.
+ * <p>
+ * <p>
+ * We consider rake to differentiate different broad faulting-types:
+ * strike-slip, reverse-faulting and normal-faulting. The classification is as
+ * follows: Strike-slip: Rake angles within (or equals) 45 to -45 degrees and
+ * 135 to -135 degrees Reverse-faulting: Rake angles within 45 to 135
+ * Normal-faulting: Rake angles within -45 to -135.
+ * <p>
+ * Notes: [1] Valid rake is in the range -180 to 180 degrees. [2] The standard
+ * deviation for area as a function of mag is given for log(area) (base-10) not
+ * area.
+ * <p>
+ * Also see: https://github.com/thingbaijam/sceqsrc
+ *
+ * </p>
+ *
+ * @version 0.0
+ */
+
+public class TMG2017CruMagAreaRel extends MagAreaRelationship {
+
+    final static String C = "TMG2017CruMagAreaRel";
+    public final static String NAME = "Thingbaijam et al.(2017)";
+
+    protected TMG2017FaultingType faultingType = NONE;
+
+    public TMG2017CruMagAreaRel(){
+        super();
+    }
+
+    public TMG2017CruMagAreaRel(double initalRake){
+        super();
+        setRake(initalRake);
+    }
+
+    /**
+     * Computes the median magnitude from rupture area for previously set rake
+     * values.
+     *
+     * @param area in km
+     * @return median magnitude MW
+     */
+    public double getMedianMag(double area) {
+
+        if (NONE == faultingType) {
+            return Double.NaN;
+        } else if (STRIKE_SLIP == faultingType) {
+            return 3.701 + 1.062 * Math.log(area) * lnToLog;
+        } else if (REVERSE_FAULTING == faultingType) {
+            return 4.158 + 0.953 * Math.log(area) * lnToLog;
+        } else {
+            return 3.157 + 1.238 * Math.log(area) * lnToLog;
+        }
+    }
+
+    /**
+     * Gives the standard deviation for the magnitude as a function of area for
+     * previously-set rake values
+     *
+     * @return standard deviation
+     */
+    public double getMagStdDev() {
+        if (NONE == faultingType) {
+            return Double.NaN;
+        } else if (STRIKE_SLIP == faultingType) {
+            return 0.184;
+        } else if (REVERSE_FAULTING == faultingType) {
+            return 0.121;
+        } else {
+            return 0.181;
+        }
+    }
+
+    /**
+     * Computes the median rupture area from magnitude (for the previously set rake
+     * values).
+     *
+     * @param mag - moment magnitude
+     * @return median area in km
+     */
+    public double getMedianArea(double mag) {
+        if (NONE == faultingType) {
+            return Double.NaN;
+        } else if (STRIKE_SLIP == faultingType) {
+            return Math.pow(10.0, -3.486 + 0.942 * mag);
+        } else if (REVERSE_FAULTING == faultingType) {
+            return Math.pow(10.0, -4.362 + 1.049 * mag);
+        } else {
+            return Math.pow(10.0, -2.551 + 0.808 * mag);
+        }
+    }
+
+    /**
+     * Computes the standard deviation of log(area) (base-10) from magnitude (for
+     * the previously set rake values)
+     *
+     * @return standard deviation
+     */
+    public double getAreaStdDev() {
+        return getMagStdDev();
+    }
+
+    /**
+     * Sets the rake.
+     *
+     * @param rake
+     */
+    public void setRake(double rake) {
+        super.setRake(rake);
+        this.faultingType = TMG2017FaultingType.fromRake(rake);
+    }
+
+    /**
+     * Returns the name of the object
+     */
+    public String getName() {
+        String type;
+        if (NONE == faultingType) {
+            type = "InvalidRake";
+        } else if (STRIKE_SLIP == faultingType) {
+            type = "Strike-Slip";
+        } else if (REVERSE_FAULTING == faultingType) {
+            type = "Reverse-Faulting";
+        } else {
+            type = "Normal-Faulting";
+        }
+        return NAME + " for crustal " + type + " events";
+    }
+}
---
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/calc/TMG2017SubMagAreaRel.java
@@ -0,0 +1,151 @@
+package nz.cri.gns.NZSHM22.opensha.calc;
+
+import org.opensha.commons.calc.magScalingRelations.MagAreaRelationship;
+import org.opensha.commons.exceptions.InvalidRangeException;
+
+import static nz.cri.gns.NZSHM22.opensha.calc.TMG2017FaultingType.*;
+
+/**
+ * <b>Title:</b>TMG2017_MagAreaRel<br>
+ *
+ * <b>Description:</b>
+ * <p>
+ * <b>Description:</b>
+ * <p>
+ * This implements SUBDUCTION specific magnitude versus rupture area relations
+ * of Thingbaijam K.K.S., P.M. Mai and K. Goda 2017, Bull. Seism. Soc. Am., 107,
+ * 2225–2246.
+ * <p>
+ * <p>
+ * We consider rake to differentiate different broad faulting-types: interface
+ * (reverse-faulting) and inslab (normal-faulting). The classification is as
+ * follows: Interface (reverse-faulting): Rake angles within 45 to 135, and
+ * Inslab (normal-faulting): Rake angles within -45 to -135.
+ * <p>
+ * Notes: [1] Relations for strike-slip events are not defined. [2] The standard
+ * deviation for area as a function of mag is given for log(area) (base-10) not
+ * area.
+ * <p>
+ * Also see: https://github.com/thingbaijam/sceqsrc
+ * </p>
+ *
+ * @version 0.0
+ */
+
+public class TMG2017SubMagAreaRel extends MagAreaRelationship {
+
+    final static String C = "TMG2017SubMagAreaRel";
+    public final static String NAME = "Thingbaijam et al.(2017)";
+
+    TMG2017FaultingType faultingType = NONE;
+
+    public TMG2017SubMagAreaRel(){
+        super();
+    }
+
+    public TMG2017SubMagAreaRel(double initialRake){
+        super();
+        setRake(initialRake);
+    }
+
+    /**
+     * Computes the median magnitude from rupture area for previously set rake
+     * values that distinguish between interface (reverse) events and inslab normal
+     * faulting events .
+     *
+     * @param area in km
+     * @return median magnitude MW
+     */
+    public double getMedianMag(double area) {
+
+        if (NONE == faultingType || STRIKE_SLIP == faultingType) {
+            return Double.NaN;
+        } else if (REVERSE_FAULTING == faultingType) {
+            // interface
+            return 3.469 + 1.054 * Math.log(area) * lnToLog;
+        } else {
+            // normal faulting
+            return 3.157 + 1.238 * Math.log(area) * lnToLog;
+        }
+    }
+
+    /**
+     * Gives the standard deviation for the magnitude as a function of area for
+     * previously-set rake and regime values
+     *
+     * @return standard deviation
+     */
+    public double getMagStdDev() {
+        if (NONE == faultingType || STRIKE_SLIP == faultingType) {
+            return Double.NaN;
+        } else if (REVERSE_FAULTING == faultingType) {
+            // interface
+            return 0.150;
+        } else {
+            // normal
+            return 0.181;
+        }
+    }
+
+    /**
+     * Computes the median rupture area from magnitude (for the previously set rake
+     * and regime values).
+     *
+     * @param mag - moment magnitude
+     * @return median area in km
+     */
+    public double getMedianArea(double mag) {
+        if (NONE == faultingType || STRIKE_SLIP == faultingType) {
+            return Double.NaN;
+        } else if (REVERSE_FAULTING == faultingType) {
+            // interface
+            return Math.pow(10.0, -3.292 + 0.949 * mag);
+        } else {
+            // inslab normal
+            return Math.pow(10.0, -2.551 + 0.808 * mag);
+        }
+    }
+
+    /**
+     * Computes the standard deviation of log(area) (base-10) from magnitude (for
+     * the previously set rake and regime values)
+     *
+     * @return standard deviation
+     */
+    public double getAreaStdDev() {
+        return getMagStdDev();
+    }
+
+    /**
+     * This overrides the parent method to disallow strike-slip
+     * faulting type.
+     *
+     * @param rake
+     */
+    public void setRake(double rake) {
+        super.setRake(rake);
+        TMG2017FaultingType type = TMG2017FaultingType.fromRake(rake);
+        if (NONE == type || STRIKE_SLIP == type) {
+            throw new InvalidRangeException(
+                    "Rake angle should be either within (45, 135) for interface or (-45, -135) for inslab-normal events"
+            );
+        } else {
+            this.faultingType = type;
+        }
+    }
+
+    /**
+     * Returns the name of the object
+     */
+    public String getName() {
+        String type;
+        if (NONE == faultingType || STRIKE_SLIP == faultingType) {
+            type = "InvalidRake";
+        } else if (REVERSE_FAULTING == faultingType) {
+            type = "Interface";
+        } else {
+            type = "Inslab-Normal";
+        }
+        return NAME + " for " + type + " events";
+    }
+}
