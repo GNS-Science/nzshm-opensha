0babf762f7879e6d907ff463a8f55e7854999ea3
Oakley Jurgens <o.jurgens@gns.cri.nz>
2024-03-25 22:29:12 +1300

    write-up

--- src/main/java/nz/cri/gns/NZSHM22/opensha/util/PythonGatewayJsonRunner.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/util/PythonGatewayJsonRunner.java
@@ -218,6 +218,7 @@ public class PythonGatewayJsonRunner {
     public static void main(String[] args) throws IOException, DocumentException {
 
         File outputDir = new File("TEST/inversions");
+        //String ruptureFile = "C:\\tmp\\rupset-coulomb-above-68-correct-params.zip";
         String ruptureFile = "C:\\Users\\user\\Downloads\\NZSHM22_RuptureSet-UnVwdHVyZUdlbmVyYXRpb25UYXNrOjEwMDAzOA==(2).zip";
         MapWithPrimitives arguments = readArguments("TEST/arguments68.json");
 
@@ -241,7 +242,7 @@ public class PythonGatewayJsonRunner {
 
         NZSHM22_ReportPageGen reportPageGen = new NZSHM22_ReportPageGen();
         reportPageGen.setName("Min Mag = 6.8")
-                .setOutputPath("/tmp/reports/m68_sampler2-max")
+                .setOutputPath("/tmp/reports/m68_nzshm22_cropped")
                 .setFillSurfaces(true)
                 .setPlotLevel(null)
                 .addPlot("SolMFDPlot")
