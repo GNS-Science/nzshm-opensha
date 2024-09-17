8a3ae7b8c70eb3c103a6165cf8ad182634d2535f
Oakley Jurgens <o.jurgens@gns.cri.nz>
2024-09-18 10:38:45 +1200

    WIP

--- src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/experimental/rsqsims/CoulombTester.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/experimental/rsqsims/CoulombTester.java
@@ -16,19 +16,35 @@ import org.opensha.sha.faultSurface.FaultSection;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
 
-public class CoulombTester {
+public class CoulombTester implements Closeable{
 
     String stiffnessCache;
     FaultSystemRupSet rupSet;
     final SectionDistanceAzimuthCalculator disAzCalc;
     StiffnessCalcModule stiffness;
     List<MultiRuptureCoulombFilter> filters = new ArrayList<>();
+    BufferedWriter writer = null;
 
     public CoulombTester(FaultSystemRupSet rupSet, String stiffnessCache) throws IOException {
+        this(rupSet, stiffnessCache, null);
+    }
+
+    public CoulombTester(FaultSystemRupSet rupSet, String stiffnessCache, File statsFile) throws IOException {
         this.rupSet = rupSet;
         this.stiffnessCache = stiffnessCache;
         this.disAzCalc = new SectionDistanceAzimuthCalculator(rupSet.getFaultSectionDataList());
+        if (statsFile != null) {
+            writer = new BufferedWriter(new FileWriter(statsFile));
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if(writer != null) {
+            writer.close();
+        }
     }
 
     void setupStiffness() {
@@ -51,10 +67,26 @@ public class CoulombTester {
         stiffness.checkUpdateStiffnessCache();
     }
 
-    public List<PlausibilityResult> applyCoulomb(MultiRuptureJump jump){
+    public int[] getStats(MultiRuptureJump jump) {
+        return filters.get(0).collectStats(jump);
+    }
+
+    public List<PlausibilityResult> applyCoulomb(MultiRuptureJump jump) {
         PlausibilityResult r0 = filters.get(0).apply(jump, false);
         PlausibilityResult r1 = filters.get(1).apply(jump, false);
         PlausibilityResult result = r0.logicalAnd(r1);
+//        if(writer != null) {
+//            try {
+//                int[] stats = filters.get(0).collectStats(jump);
+//                for (int stat : stats) {
+//                    writer.write(stat + ", ");
+//                }
+//                writer.write("\n");
+//            }catch(IOException x) {
+//                throw new RuntimeException(x);
+//            }
+//        }
+
         return List.of(r0, r1, result);
     }
 
