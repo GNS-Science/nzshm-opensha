5bb3eda299d69fe763a60a4c917c6c26888d0a16
Oakley Jurgens <o.jurgens@gns.cri.nz>
2025-07-24 15:22:57 +1200

    formatting

--- src/test/java/nz/cri/gns/NZSHM22/opensha/griddedSeismicity/NZSHM22_GridSourceGeneratorTest.java
+++ src/test/java/nz/cri/gns/NZSHM22/opensha/griddedSeismicity/NZSHM22_GridSourceGeneratorTest.java
@@ -2,13 +2,12 @@ package nz.cri.gns.NZSHM22.opensha.griddedSeismicity;
 
 import static org.junit.Assert.*;
 
-import nz.cri.gns.NZSHM22.opensha.data.region.NewZealandRegions;
-import org.junit.Test;
-import org.opensha.commons.geo.Location;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import nz.cri.gns.NZSHM22.opensha.data.region.NewZealandRegions;
+import org.junit.Test;
+import org.opensha.commons.geo.Location;
 
 public class NZSHM22_GridSourceGeneratorTest {
 
@@ -22,7 +21,8 @@ public class NZSHM22_GridSourceGeneratorTest {
                 NZSHM22_GriddedData.fromFile("seismicityGrids/normalFocalMech.grid");
 
         List<Location> focalGridLocations = new ArrayList<>();
-        // using NZ_RECTANGLE_GRIDDED here because it's larger than the data, so that we don't miss any
+        // using NZ_RECTANGLE_GRIDDED here because it's larger than the data, so that we don't miss
+        // any
         for (Location location : new NewZealandRegions.NZ_RECTANGLE_GRIDDED()) {
             Double strike = strikeGrid.getValue(location);
             Double reverse = reverseGrid.getValue(location);
@@ -32,7 +32,7 @@ public class NZSHM22_GridSourceGeneratorTest {
             value += reverse == null ? 0 : reverse;
             value += normal == null ? 0 : normal;
             assertTrue(value == 1 || value == 0);
-            if(value == 1) {
+            if (value == 1) {
                 focalGridLocations.add(location);
             }
         }
@@ -44,7 +44,7 @@ public class NZSHM22_GridSourceGeneratorTest {
         assertEquals(nzRegion.size(), focalGridLocations.size());
 
         // assert that the NZ region is exactly the region covered by the focal mech grids
-        for(int i = 0 ; i < nzRegion.size(); i++) {
+        for (int i = 0; i < nzRegion.size(); i++) {
             assertEquals(nzRegion.get(i), focalGridLocations.get(i));
         }
     }
