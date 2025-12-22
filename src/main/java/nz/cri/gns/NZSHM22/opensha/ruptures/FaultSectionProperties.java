93b1d6ccb92d829150acf817c290fa610da610b7
voj <o.jurgens@gns.cri.nz>
2025-12-22 16:17:24 +1300

    documentation

--- src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/FaultSectionProperties.java
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/ruptures/FaultSectionProperties.java
@@ -9,12 +9,21 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import org.opensha.commons.util.modules.helpers.FileBackedModule;
 
+/** A module to side-load additional fault section properties */
 public class FaultSectionProperties implements FileBackedModule {
 
     protected List<Map<String, Object>> data = new ArrayList<>();
 
     public FaultSectionProperties() {}
 
+    /**
+     * Set a property on a fault section. It is expected but not policed that all values of a
+     * property have the same type.
+     *
+     * @param sectionId the section id
+     * @param property the property name
+     * @param value the value
+     */
     public void set(int sectionId, String property, Object value) {
         while (data.size() <= sectionId) {
             data.add(null);
@@ -27,6 +36,12 @@ public class FaultSectionProperties implements FileBackedModule {
         properties.put(property, value);
     }
 
+    /**
+     * Gets all properties for a specific fault section.
+     *
+     * @param sectionId the fault section id
+     * @return a Map of properties, or null if no properties are set on the section
+     */
     public Map<String, Object> get(int sectionId) {
         if (data.size() < sectionId + 1) {
             return null;
@@ -34,6 +49,13 @@ public class FaultSectionProperties implements FileBackedModule {
         return data.get(sectionId);
     }
 
+    /**
+     * Gets a property for a specific fault section.
+     *
+     * @param sectionId the section id
+     * @param property the property name
+     * @return the value or null if the property has not been set on the section
+     */
     public Object get(int sectionId, String property) {
         Map<String, Object> properties = get(sectionId);
         if (properties != null) {
