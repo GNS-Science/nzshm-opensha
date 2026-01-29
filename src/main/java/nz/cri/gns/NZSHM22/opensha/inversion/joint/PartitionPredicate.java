b22eca85ee0877d951d68e87372c9f40c99b3f23
voj <o.jurgens@gns.cri.nz>
2026-01-29 16:44:26 +1300

    [418] fault section properties

---
+++ src/main/java/nz/cri/gns/NZSHM22/opensha/inversion/joint/PartitionPredicate.java
@@ -0,0 +1,44 @@
+package nz.cri.gns.NZSHM22.opensha.inversion.joint;
+
+import java.util.function.IntPredicate;
+import nz.cri.gns.NZSHM22.opensha.ruptures.FaultSectionProperties;
+import org.opensha.sha.earthquake.faultSysSolution.FaultSystemRupSet;
+
+public enum PartitionPredicate {
+    TVZ,
+    SANS_TVZ,
+    CRUSTAL,
+    HIKURANGI,
+    PUYSEGUR;
+
+    public IntPredicate getPredicate(FaultSystemRupSet ruptureSet) {
+        FaultSectionProperties extraProperties =
+                ruptureSet.requireModule(FaultSectionProperties.class);
+
+        switch (this) {
+            case TVZ:
+                return (sectionId) -> extraProperties.get(sectionId, TVZ.name()) == Boolean.TRUE;
+            case SANS_TVZ:
+                return (sectionId) ->
+                        extraProperties.get(sectionId, SANS_TVZ.name()) == Boolean.TRUE;
+            case CRUSTAL:
+                return (sectionId) ->
+                        extraProperties.get(sectionId, CRUSTAL.name()) == Boolean.TRUE;
+            case HIKURANGI:
+                return (sectionId) ->
+                        extraProperties.get(sectionId, HIKURANGI.name()) == Boolean.TRUE;
+            case PUYSEGUR:
+                return (sectionId) ->
+                        extraProperties.get(sectionId, PUYSEGUR.name()) == Boolean.TRUE;
+        }
+        throw new IllegalStateException("Unknown RegionPredicate");
+    }
+
+    public boolean isSubduction() {
+        return (this == PUYSEGUR || this == HIKURANGI);
+    }
+
+    public boolean isCrustal() {
+        return !isSubduction();
+    }
+}
